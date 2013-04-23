9a2f2851a4267d0c18052db360d95e1a9106b477
Thomas A. Gerds <tag@biostat.ku.dk>
2013-04-23 00:00:00 +0000

    version 1.3.5

---
+++ Examples/auto/FiguresKeidingAndersen.el
@@ -0,0 +1,26 @@
+(TeX-add-style-hook "FiguresKeidingAndersen"
+ (lambda ()
+    (TeX-run-style-hooks
+     "listings"
+     "color"
+     "hyperref"
+     "amssymb"
+     "latexsym"
+     "wasysym"
+     "marvosym"
+     "textcomp"
+     "soul"
+     "wrapfig"
+     "float"
+     "longtable"
+     "graphicx"
+     "fixltx2e"
+     "fontenc"
+     "T1"
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "art11"
+     "article"
+     "11pt")))
+
